d7e558b3ba6c8d0ac58039ac47765e5158e60c16
homanok8 <homanok8@gamil.com>
2021-06-04 11:11:19 +0900

    리스트 메뉴 부분 스타일 #24

--- src/components/listNav/ListNav.js
+++ src/components/listNav/ListNav.js
@@ -1,6 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
+const langListNav = {
+    ko:{
+        //고정
+        listName:["질문/답변","알록달록","NEST","활동"]
+    }
+}
+function ListNav() {
+    const listName = langListNav.ko.listName;
+    const listMenu = [
+        {
+            id:1,
+            name: listName[0],
+            href: '/'
+        },
+        {
+            id:2,
+            name: listName[1],
+            href: '/alog'
+        },
+        {
+            id:3,
+            name: listName[2],
+            href: '/nest'
+        },
+        {
+            id:4,
+            name: listName[3],
+            href: '/activity'
+        }
+    ];
+    const [highlight, setHighlight] = useState(0);
+    return (
+        <NavGroup>
+            {listMenu.map((list, index) => (
+                <ListEl 
+                    list={list} 
+                    key={list.id} 
+                    highlight={highlight} setHighlight={setHighlight}
+                    selected={index}
+                />
+            ))}
+        </NavGroup>
+    );
+}
+
+function ListEl(props) {
+    const [place, setPlace] = useState(false);
+    useEffect(()=>{
+        if (props.highlight === props.selected) {
+            setPlace(true);
+        } else {
+            setPlace(false);
+        }
+    }, [props.highlight]);
+
+    return (
+        <NavList place={place} onClick={()=>{props.setHighlight(props.selected);}}>
+            <Link to={props.list.href || '/'}>{props.list.name}</Link>
+        </NavList>
+    );
+}
 
 const NavGroup = styled.nav`
     height: 50px;
@@ -20,12 +82,23 @@ const NavList = styled.li`
     cursor : pointer;
     font-size: 0.875rem;
     font-weight:bold;
+    background-color: ${ props => props.place ? '#f5f5f5' : '#fff'};
+    box-shadow: ${ props => props.place ? 'inset 3px 3px 3px #fff, inset -1px -1px 2px #fefefe':'inherit'};
     transition:0.3s;
 
     :active, :hover{
-        font-weight: bold;
         background-color: #f5f5f5;
         box-shadow:inset 3px 3px 3px #fff, inset -1px -1px 2px #fefefe; 
+    }
+    a {
+        color:${props => props.place ? '#fd0031' : '#707070'};
+        text-decoration:none;
+        display:block;
+        transition:0.3s;
+
+        :active, :hover{
+            color: #fd0031;
+        }
     }
 
     @media (max-width:360px){
@@ -33,40 +106,6 @@ const NavList = styled.li`
     }
 `;
 
-const Anchor = styled(Link)`
-    color:#707070;
-    text-decoration:none;
 
-    :active, :hover{
-        color: #fd0031;
-    }
-`;
-
-const langListNav = {
-    ko:{
-        //고정
-        listName:["질문/답변","알록달록","NEST","활동"]
-    }
-}
-const ListNav = () => {
-    const listName = langListNav.ko.listName;
-
-    return(
-        <NavGroup>
-            <NavList> 
-                <Anchor to="/">{listName[0]}</Anchor>
-            </NavList>
-            <NavList> 
-                <Anchor to="/alog">{listName[1]}</Anchor>
-            </NavList>
-            <NavList> 
-                <Anchor to="/nest">{listName[2]}</Anchor>
-            </NavList>
-            <NavList> 
-                <Anchor to="/activity">{listName[3]}</Anchor>
-            </NavList>
-        </NavGroup>
-    );
-}
 
 export default ListNav;
