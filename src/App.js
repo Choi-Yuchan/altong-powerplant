9723015173c563333a34cb901d759b797e78a219
homanok8 <homanok8@gamil.com>
2021-06-15 18:03:05 +0900

    폰트 스타일 적용 #23

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import Alog from './routes/Alog';
 import Nest from './routes/Nest';
 import Activity from './routes/Activity';
 import Section from './routes/Section';
-import { Font } from './style/Font';
+import './style/font.css';
 
 function App() {
 
   return (
     <Wrap>
-      <Font/>
       <TopNavi/>
       <Profile/>
       <ListNav/>
