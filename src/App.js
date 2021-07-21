646f6f75aabf5af7a0a8243e52e0e0fe38e71869
Choi-Yuchan <cor330@gmail.com>
2021-07-21 17:20:39 +0900

    Add a new page to load a korean page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import PlantKR from './components/PlantKR';
 import PlantEN from './components/PlantEN';
 import PlantJA from './components/PlantJA';
 import PlantZH from './components/PlantZH';
+import DefaultApp from './components/DefaultApp';
 
 
 function App() {
 
   return (
       <BrowserRouter> 
-        <Route exact path="/" component={PlantKR}></Route>
+        <Route exact path="/" component={DefaultApp}></Route>
         <Switch>
+          <Route path="/ko" component={PlantKR}></Route>
           <Route path="/en" component={PlantEN}></Route>
           <Route path="/ja" component={PlantJA}></Route>
           <Route path="/zh" component={PlantZH}></Route>
--- src/components/alog/AlogTitle.js
+++ src/components/alog/AlogTitle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-function AlogTitle ({id, subText, userShortCut, shortCut, direct}) {
+function AlogTitle ({id, subText, userShortCut, shortCut}) {
 
     return (
         <AlogHeader>
